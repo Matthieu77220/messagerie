7867770b1a1648168c28a1af1f8078da6ae7f193
Matthieu_772 <matthieu.dietrich77@gmail.com>
2025-12-17 15:29:05 +0100

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { createServer } = require('node:http');
+const {Server} = require('socket.io')
+const cors = require('cors')
+
+const app = express();
+const server = createServer(app);
+
+const corsOptions = {
+    origin : "*",
+    methods : ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders : ["Content-Type", "Authorization"]
+}
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",   
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders : ["Content-Type", "Authorization"]
+  }
+})
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('<h1>Hello world</h1>');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+  });
+});
+
+    
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
